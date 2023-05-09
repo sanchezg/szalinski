fceecfa9fe0b32dff5491bc765c69308ed41e351
Gonzalo Sanchez <gonzalosanchezmarques@gmail.com>
2023-05-09 09:50:56 -0300

    Use redis for storage

--- src/application/controllers.py
+++ src/application/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request
 
 
 main = Blueprint("main", __name__, template_folder="templates")
@@ -9,7 +9,7 @@ def index(code=None):
     if request.method == "GET":
         if code is None:
             return render_template("index.html")
-        # else: redirect to hashed code
-        render_template("index.html")
+        # else: get url and redirect to hashed code
+        return redirect("https://www.google.com/")
     url = request.form.get("url")
     return render_template("index.html", url_hash=url)
