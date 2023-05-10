b948be2395633135d8258a231a763858d3947f31
Gonzalo Sanchez <gonzalosanchezmarques@gmail.com>
2023-05-10 00:43:10 -0300

    Tests: missing fake repo

---
+++ tests/fakes.py
@@ -0,0 +1,15 @@
+from src.domain.interfaces import AbstractRepo
+from src.domain.model import URL
+
+
+class FakeRepo(AbstractRepo):
+    def __init__(self) -> None:
+        self._data = {}
+        super().__init__()
+
+    async def get(self, url_hash):
+        return self._data.get(url_hash)
+    
+    async def insert(self, url_hash, url):
+        self._data[url_hash] = URL(url_hash=url_hash, url=url)
+        return url_hash
