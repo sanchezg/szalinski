5a314b69097445bcaeb4eb7a7d204399039d29b5
Gonzalo Sanchez <gonzalosanchezmarques@gmail.com>
2023-05-15 11:34:57 -0300

    Improve template response on index page

--- src/application/controllers.py
+++ src/application/controllers.py
@@ -1,16 +1,30 @@
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends, HTTPException, responses, status
+from fastapi import APIRouter, Depends, HTTPException, Request, responses, status
+from fastapi.templating import Jinja2Templates
 
+from src import settings
 from src.container import Container
 from src.domain.model import URL
 from src.domain.services.urls import UrlLookup, UrlStore
 
 router = APIRouter()
+templates = Jinja2Templates(directory="src/application/templates")
 
 
-@router.get("/")
-async def index():
-    return "Hello world, I'm Szalinski URL shortener"
+@router.get("/", response_class=responses.HTMLResponse)
+@inject
+async def index(
+    request: Request,
+    url: str = None,
+    url_store: UrlStore = Depends(Provide[Container.url_store]),
+):
+    if url is None:
+        return templates.TemplateResponse("index.html", context={"request": {}})
+    url_hash = await url_store(url=url)
+    return templates.TemplateResponse(
+        "index.html",
+        context={"request": request, "url_hash": f"{settings.BASE_URL}/{url_hash}"},
+    )
 
 
 @router.get("/{code}")
@@ -22,9 +36,7 @@ async def get_url(
     url = await url_lookup(url_hash=code)
     if url:
         return responses.RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="URL hash not found"
-    )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="URL hash not found")
 
 
 @router.post("/", response_model=URL)
